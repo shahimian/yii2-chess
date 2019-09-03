f5e7e1acfee4a25535e5d60dd0f4702a87ba5b09
a.shahimian <shahimian@gmail.com>
2019-09-03 13:38:54 +0430

    view chess board and its accessories

---
+++ Assets.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace shahimian\chess;
+
+use yii\web\AssetBundle;
+
+class Assets extends AssetBundle {
+
+    public $sourcePath = '@vendor/shahimian/yii2-chess/assets/dist';
+
+    public $css = [
+        'styles.css',
+    ];
+
+}
